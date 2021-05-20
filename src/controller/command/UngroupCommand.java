13cdb4e28944d1b6ac5b77e0cb7f1c980cd6f778
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2021-05-20 00:05:06 -0500

    Connect group and ungroup command to ui:

--- src/controller/command/UngroupCommand.java
+++ src/controller/command/UngroupCommand.java
@@ -1,10 +1,10 @@
 package controller.command;
 
-public class GroupCommand implements ICommand, IUndoable {
+public class UngroupCommand implements ICommand, IUndoable {
 
   @Override
   public void run() {
-    System.out.println("Group");
+    System.out.println("Ungroup");
     CommandHistory.add(this);
   }
 
